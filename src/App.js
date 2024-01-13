2d6e2941c30b0fbdc75da5f0e9fd7ae1a7b90d52
mcisneros <ext_mangcisneross@falabella.cl>
2024-01-13 02:22:57 -0500

    feat: Implement default page and routing
    Default route is being implemented so whenever a user gets / it will be
    redirected to /demo. On the other side, few routing more was implemented
    in order to test routing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import SpeedControlComponent from "./components/SpeedControlComponent";
 import SpeedIndicatorComponent from "./components/SpeedIndicatorComponent";
 import IconComponent from "./components/IconComponent";
+import {BrowserRouter as Router, Link, Navigate, Route, Routes} from 'react-router-dom';
+import React from "react";
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <header className="App-header">
@@ -26,4 +28,54 @@ function App() {
     );
 }
 
-export default App;
+function Root() {
+    return (
+        <>
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to='/demo'>Demo</Link>
+                        </li>
+                        <li>
+                            <Link to='/test'>Test</Link>
+                        </li>
+                        <li>
+                            <Link to='/test2'>Test2</Link>
+                        </li>
+                    </ul>
+                </div>
+                <Routes>
+                    <Route path='/demo' element={
+                        <>
+                            <App/>
+                            <h2>Footer Demo</h2>
+                        </>
+                    }/>
+                    <Route path='/test' element={
+                        <>
+                            <App/>
+                            <h2>Footer Test</h2>
+                        </>
+                    }/>
+                    <Route path='/test2' element={
+                        <>
+                            <App/>
+                            <h2>Footer Test 2</h2>
+                        </>
+                    }/>
+                    <Route path={'/'} element={
+                        <Navigate to={'/demo'}/>
+                    }/>
+                    <Route path={'*'} element={
+                        <>
+                            <h1>Not found</h1>
+                        </>
+                    }/>
+                </Routes>
+            </Router>
+        </>
+    );
+}
+
+export default Root;
